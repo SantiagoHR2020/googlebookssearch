1749530bba5c0c54fc4a01866d61e66c38b1ebf5
Santiago Hincapie <santiagohr81@hotmail.com>
2021-02-17 16:32:20 -0500

    seerver bug corrected

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchPage from './pages/searchPage'
+import NavBar from './component/navBar'
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
       <SearchPage />
     </div>
   );
--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
